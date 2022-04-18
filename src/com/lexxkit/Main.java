2769e7dad04015aad186a77c5d71dca75c176dde
Lexxkit <lexxkit@gmail.com>
2022-04-18 13:23:42 +0100

    Completed tasks 1 and 2.

--- src/com/lexxkit/Main.java
+++ src/com/lexxkit/Main.java
@@ -1,8 +1,55 @@
 package com.lexxkit;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Task 1
+        System.out.println("Task 1. Check if a year is a leap year.\n");
+
+        int myYear = 1985;
+        printYearCharacteristic(myYear);
+
+        // Task 2
+        System.out.println("\nTask 2. Which app version - Lite or Full?\n");
+        int clientDeviceYear = 2020;
+        int clientOS = 0;
+        printAppType(clientOS, clientDeviceYear);
     }
+
+    public static void printYearCharacteristic(int year) {
+
+        boolean isLeapYear = year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is an ordinary year.");
+        }
+    }
+
+    public static void printAppType(int clientOS, int deviceYear) {
+        boolean isPhoneNew = isDeviceNew(deviceYear);
+        String clientOSName = getClientOSName(clientOS);
+
+        if (isPhoneNew) {
+            System.out.println("Install " + clientOSName + " application from the link.");
+        } else {
+            System.out.println("Install light version of " + clientOSName + " application from the link.");
+        }
+    }
+
+    public static boolean isDeviceNew(int deviceYear) {
+        int currentDate = LocalDate.now().getYear();
+        return deviceYear >= currentDate;
+    }
+
+    public static String getClientOSName(int clientOSCode) {
+        if (clientOSCode == 0) {
+            return "iOS";
+        }
+        return "Android";
+    }
+
 }
